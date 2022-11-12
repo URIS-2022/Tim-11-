10705193bd2486e9f967d1d26638a51e043fef2d
Jovana Bobic <jovanabobic15@gmail.com>
2022-11-12 22:40:18 +0100

    c1

--- WebVella.Erp.Plugins.Mail/MailPlugin.20190419.cs
+++ WebVella.Erp.Plugins.Mail/MailPlugin.20190419.cs
@@ -300,7 +300,7 @@ namespace WebVella.Erp.Plugins.Mail
 				var url = "/sdk/server/job/l/plan";
 				var iconClass = "fa fa-cogs";
 				var weight = 1;
-				var type = ((int)3);
+				var type = 3;
 				var access = new List<Guid>();
 				var entityListPages = new List<Guid>();
 				var entityCreatePages = new List<Guid>();
