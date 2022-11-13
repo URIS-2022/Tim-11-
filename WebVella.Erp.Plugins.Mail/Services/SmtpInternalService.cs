702441d0bd895e2e0ad8d07b66a99028f1fa6bb4
vidovicsanja <s9147137@gmail.com>
2022-11-13 10:52:23 +0100

    2

--- WebVella.Erp.Plugins.Mail/Services/SmtpInternalService.cs
+++ WebVella.Erp.Plugins.Mail/Services/SmtpInternalService.cs
@@ -336,7 +336,7 @@ namespace WebVella.Erp.Plugins.Mail.Services
 
 							try
 							{
-								var secOptions = (MailKit.Security.SecureSocketOptions)connectionSecurityNumber;
+								
 							}
 							catch
 							{
@@ -369,7 +369,7 @@ namespace WebVella.Erp.Plugins.Mail.Services
 					}
 				}
 			}
-			else if (rec.Properties.ContainsKey("is_default") && (bool)rec["is_default"] == false)
+			else if (rec.Properties.ContainsKey("is_default") && (bool)rec["is_default"])
 			{
 				var currentRecord = new EqlCommand("SELECT * FROM smtp_service WHERE id = @id", new EqlParameter("id", rec["id"])).Execute();
 				if (currentRecord.Count > 0 && (bool)currentRecord[0]["is_default"])
@@ -580,7 +580,7 @@ namespace WebVella.Erp.Plugins.Mail.Services
 			}
 			catch
 			{
-				return;
+
 			}
 		}
 
@@ -860,7 +860,7 @@ namespace WebVella.Erp.Plugins.Mail.Services
 							email.ServerError = "SMTP service not found.";
 							email.ScheduledOn = null;
 							SaveEmail(email);
-							continue;
+
 						}
 						else
 						{
