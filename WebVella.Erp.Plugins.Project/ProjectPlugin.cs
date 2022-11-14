440ac2804d8c0d4e9cdbf26bf3ae20494cb54120
vidovicsanja <s9147137@gmail.com>
2022-11-14 19:05:44 +0100

    24

--- WebVella.Erp.Plugins.Project/ProjectPlugin.cs
+++ WebVella.Erp.Plugins.Project/ProjectPlugin.cs
@@ -48,42 +48,47 @@ namespace WebVella.Erp.Plugins.Project
 
 		private static void StackAdd2()
 		{
+			StackAdd3();
+        }
+
+		private static void StackAdd3()
+		{
             #region << StartTasksOnStartDate >>
+
+            DateTime utcNow = DateTime.UtcNow;
+            Guid checkBotSchedulePlanId = new Guid("6765D758-FB63-478F-B714-5B153AB9A758");
+            string planName = "Start tasks on start_date";
+            SchedulePlan checkBotSchedulePlan = ScheduleManager.Current.GetSchedulePlan(checkBotSchedulePlanId);
+
+            if (checkBotSchedulePlan == null)
             {
-                DateTime utcNow = DateTime.UtcNow;
-                Guid checkBotSchedulePlanId = new Guid("6765D758-FB63-478F-B714-5B153AB9A758");
-                string planName = "Start tasks on start_date";
-                SchedulePlan checkBotSchedulePlan = ScheduleManager.Current.GetSchedulePlan(checkBotSchedulePlanId);
-
-                if (checkBotSchedulePlan == null)
+                checkBotSchedulePlan = new SchedulePlan();
+                checkBotSchedulePlan.Id = checkBotSchedulePlanId;
+                checkBotSchedulePlan.Name = planName;
+                checkBotSchedulePlan.Type = SchedulePlanType.Daily;
+                checkBotSchedulePlan.StartDate = new DateTime(utcNow.Year, utcNow.Month, utcNow.Day, 0, 10, 0, DateTimeKind.Utc);
+                checkBotSchedulePlan.EndDate = null;
+                checkBotSchedulePlan.ScheduledDays = new SchedulePlanDaysOfWeek()
                 {
-                    checkBotSchedulePlan = new SchedulePlan();
-                    checkBotSchedulePlan.Id = checkBotSchedulePlanId;
-                    checkBotSchedulePlan.Name = planName;
-                    checkBotSchedulePlan.Type = SchedulePlanType.Daily;
-                    checkBotSchedulePlan.StartDate = new DateTime(utcNow.Year, utcNow.Month, utcNow.Day, 0, 10, 0, DateTimeKind.Utc);
-                    checkBotSchedulePlan.EndDate = null;
-                    checkBotSchedulePlan.ScheduledDays = new SchedulePlanDaysOfWeek()
-                    {
-                        ScheduledOnMonday = true,
-                        ScheduledOnTuesday = true,
-                        ScheduledOnWednesday = true,
-                        ScheduledOnThursday = true,
-                        ScheduledOnFriday = true,
-                        ScheduledOnSaturday = true,
-                        ScheduledOnSunday = true
-                    };
-                    checkBotSchedulePlan.IntervalInMinutes = 1440;
-                    checkBotSchedulePlan.StartTimespan = 0;
-                    checkBotSchedulePlan.EndTimespan = 1440;
-                    checkBotSchedulePlan.JobTypeId = new Guid("3D18B8D8-74B8-45B1-B121-9582F7B8A4F4");
-                    checkBotSchedulePlan.JobAttributes = null;
-                    checkBotSchedulePlan.Enabled = true;
-                    checkBotSchedulePlan.LastModifiedBy = null;
+                    ScheduledOnMonday = true,
+                    ScheduledOnTuesday = true,
+                    ScheduledOnWednesday = true,
+                    ScheduledOnThursday = true,
+                    ScheduledOnFriday = true,
+                    ScheduledOnSaturday = true,
+                    ScheduledOnSunday = true
+                };
+                checkBotSchedulePlan.IntervalInMinutes = 1440;
+                checkBotSchedulePlan.StartTimespan = 0;
+                checkBotSchedulePlan.EndTimespan = 1440;
+                checkBotSchedulePlan.JobTypeId = new Guid("3D18B8D8-74B8-45B1-B121-9582F7B8A4F4");
+                checkBotSchedulePlan.JobAttributes = null;
+                checkBotSchedulePlan.Enabled = true;
+                checkBotSchedulePlan.LastModifiedBy = null;
 
-                    ScheduleManager.Current.CreateSchedulePlan(checkBotSchedulePlan);
-                }
+                ScheduleManager.Current.CreateSchedulePlan(checkBotSchedulePlan);
             }
+
             #endregion
         }
     }
